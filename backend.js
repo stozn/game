d96e74b07cdb04f353c13bbf365c368b407fd552
stozn <stozn.cn@outlook.com>
2024-05-24 12:43:15 +0800

    ko

--- backend.js
+++ backend.js
@@ -12,7 +12,7 @@ const port = 3000
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+    res.sendFile(__dirname + '/index.html')
 })
 
 const { RectCollider, CircleCollider } = require('./public/js/classes/Collider.js')
@@ -37,97 +37,97 @@ colliders.push(new RectCollider(700, 750, 50, 300))
 const SPEED = 10
 
 io.on('connection', (socket) => {
-  console.log('a user connected')
+    console.log('a user connected')
 
-  socket.on('initGame', ({ username, width, height }) => {
-    const playerColor = Math.random() * 360
-    var positionValid = false;
-    var x = width * Math.random()
-    var y = height * Math.random()
-    while (!positionValid) {
-      positionValid = true;
-      colliders.forEach(element => {
-        if (element.isColliding(x, y)) {
-          positionValid = false;
+    socket.on('initGame', ({ username, width, height }) => {
+        const playerColor = Math.random() * 360
+        var positionValid = false;
+        var x = width * Math.random()
+        var y = height * Math.random()
+        while (!positionValid) {
+            positionValid = true;
+            colliders.forEach(element => {
+                if (element.isColliding(x, y)) {
+                    positionValid = false;
+                }
+            })
+            if (!positionValid) {
+                x = width * Math.random()
+                y = height * Math.random()
+            }
         }
-      })
-      if (!positionValid) {
-        x = width * Math.random()
-        y = height * Math.random()
-      }
-    }
-    backEndPlayers[socket.id] = {
-      x,
-      y,
-      angle: 0,
-      color: `hsl(${playerColor}, 100%, 50%)`,
-      score: 0,
-      username
-    }
+        backEndPlayers[socket.id] = {
+            x,
+            y,
+            angle: 0,
+            color: `hsl(${playerColor}, 100%, 50%)`,
+            score: 0,
+            username
+        }
 
-    backEndPlayers[socket.id].canvas = {
-      width,
-      height,
-    }
-  })
+        backEndPlayers[socket.id].canvas = {
+            width,
+            height,
+        }
+    })
 
-  socket.on('disconnect', (reason) => {
-    console.log(reason)
-    delete backEndPlayers[socket.id]
-  })
+    socket.on('disconnect', (reason) => {
+        console.log(reason)
+        delete backEndPlayers[socket.id]
+    })
 
-  socket.on('updatePlayers', ({ x, y, angle }) => {
-    if (backEndPlayers[socket.id]) {
-      backEndPlayers[socket.id].x = x
-      backEndPlayers[socket.id].y = y
-      backEndPlayers[socket.id].angle = angle
-    }
-  })
+    socket.on('updatePlayers', ({ x, y, angle }) => {
+        if (backEndPlayers[socket.id]) {
+            backEndPlayers[socket.id].x = x
+            backEndPlayers[socket.id].y = y
+            backEndPlayers[socket.id].angle = angle
+        }
+    })
 
-  socket.on('shoot', ({ x, y, angle }) => {
-    const velocity = {
-      x: SPEED * Math.sin(angle),
-      y: SPEED * Math.cos(angle)
-    }
-    const backEndProjectile = {
-      x,
-      y,
-      velocity,
-      playerId: socket.id
-    }
-    io.emit('addProjectile', backEndProjectile)
-  })
+    socket.on('shoot', ({ x, y, angle }) => {
+        const velocity = {
+            x: SPEED * Math.sin(angle),
+            y: SPEED * Math.cos(angle)
+        }
+        const backEndProjectile = {
+            x,
+            y,
+            velocity,
+            playerId: socket.id
+        }
+        io.emit('addProjectile', backEndProjectile)
+    })
 
-  socket.on('hit', (playerId) => {
-    if (backEndPlayers[playerId]) {
-      backEndPlayers[playerId].score++
-    }
-    if (backEndPlayers[socket.id]) {
-      delete backEndPlayers[socket.id]
-    }
-    console.log('hit', playerId, socket.id)
-  })
+    socket.on('hit', (playerId) => {
+        if (backEndPlayers[playerId]) {
+            backEndPlayers[playerId].score++
+        }
+        if (backEndPlayers[socket.id]) {
+            delete backEndPlayers[socket.id]
+        }
+        console.log('hit', playerId, socket.id)
+    })
 
-  socket.on('chatPublic', (inputText) => {
-    backEndChatMessages[socket.id] = inputText;
-    clearTimeout(chatMessageBubbleTimers[socket.id])
-    chatMessageBubbleTimers[socket.id] = setTimeout(() => {
-      delete backEndChatMessages[socket.id]
-      io.emit('updateChatMessages', backEndChatMessages)
-    }, 6000)
-    io.emit('updateChatMessages', backEndChatMessages)
-    io.emit('updateChatMessagePool', { id: socket.id, inputText })
-  })
+    socket.on('chatPublic', (inputText) => {
+        backEndChatMessages[socket.id] = inputText;
+        clearTimeout(chatMessageBubbleTimers[socket.id])
+        chatMessageBubbleTimers[socket.id] = setTimeout(() => {
+            delete backEndChatMessages[socket.id]
+            io.emit('updateChatMessages', backEndChatMessages)
+        }, 6000)
+        io.emit('updateChatMessages', backEndChatMessages)
+        io.emit('updateChatMessagePool', { id: socket.id, inputText })
+    })
 })
 
 // backend ticker
 setInterval(() => {
-  io.emit('updatePlayers', backEndPlayers)
-  io.emit('updateChatMessages', backEndChatMessages)
+    io.emit('updatePlayers', backEndPlayers)
+    io.emit('updateChatMessages', backEndChatMessages)
 }, 50)
 
 server.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
 
 console.log('server loaded')
--- public/js/frontend.js
+++ public/js/frontend.js
@@ -14,8 +14,8 @@ var frontEndChatMessages
 const frontEndChatMessagePool = []
 
 const map = {
-  width: 5000,
-  height: 5000
+    width: 5000,
+    height: 5000
 }
 
 const colliders = []
@@ -52,245 +52,245 @@ colliders.push(new CircleCollider(-300, 600, 50))
 
 
 socket.on('connect', () => {
-  socket.emit('initCanvas', {
-    width: canvas.width,
-    height: canvas.height,
-  })
+    socket.emit('initCanvas', {
+        width: canvas.width,
+        height: canvas.height,
+    })
 })
 
 let projectileId = 0
 // only update position of the projectiles
 socket.on('addProjectile', (backEndProjectile) => {
-  frontEndProjectiles[projectileId] = new Projectile({
-    x: backEndProjectile.x,
-    y: backEndProjectile.y,
-    radius: 5,
-    color: frontEndPlayers[backEndProjectile.playerId]?.color,
-    velocity: backEndProjectile.velocity,
-    playerId: backEndProjectile.playerId
-  })
-  projectileId++;
+    frontEndProjectiles[projectileId] = new Projectile({
+        x: backEndProjectile.x,
+        y: backEndProjectile.y,
+        radius: 5,
+        color: frontEndPlayers[backEndProjectile.playerId]?.color,
+        velocity: backEndProjectile.velocity,
+        playerId: backEndProjectile.playerId
+    })
+    projectileId++;
 })
 // only update position and angle of the players
 socket.on('updatePlayers', (backEndPlayers) => {
-  for (const id in backEndPlayers) {
-    const backEndPlayer = backEndPlayers[id]
+    for (const id in backEndPlayers) {
+        const backEndPlayer = backEndPlayers[id]
 
-    if (!frontEndPlayers[id]) {
-      frontEndPlayers[id] = new Player({
-        x: backEndPlayer.x,
-        y: backEndPlayer.y,
-        angle: backEndPlayer.angle,
-        color: backEndPlayer.color,
-        username: backEndPlayer.username
-      })
-      // reset key
-      keys.w.pressd = false
-      keys.a.pressd = false
-      keys.s.pressd = false
-      keys.d.pressd = false
-      document.querySelector('#playerLabels').innerHTML +=
-        `<div data-id="${id}" data-score="${backEndPlayer.score}">${backEndPlayer.username}: ${backEndPlayer.score}</div>`
-    } else {
-      document.querySelector(`div[data-id="${id}"]`).innerHTML = `${backEndPlayer.username}: ${backEndPlayer.score}`
-      document.querySelector(`div[data-id="${id}"]`).setAttribute('data-score', backEndPlayer.score)
-      const parenetDiv = document.querySelector('#playerLabels')
-      const childDivs = Array.from(parenetDiv.querySelectorAll('div'))
-      childDivs.sort((a, b) => {
-        return b.getAttribute('data-score') - a.getAttribute('data-score')
-      })
+        if (!frontEndPlayers[id]) {
+            frontEndPlayers[id] = new Player({
+                x: backEndPlayer.x,
+                y: backEndPlayer.y,
+                angle: backEndPlayer.angle,
+                color: backEndPlayer.color,
+                username: backEndPlayer.username
+            })
+            // reset key
+            keys.w.pressd = false
+            keys.a.pressd = false
+            keys.s.pressd = false
+            keys.d.pressd = false
+            document.querySelector('#playerLabels').innerHTML +=
+                `<div data-id="${id}" data-score="${backEndPlayer.score}">${backEndPlayer.username}: ${backEndPlayer.score}</div>`
+        } else {
+            document.querySelector(`div[data-id="${id}"]`).innerHTML = `${backEndPlayer.username}: ${backEndPlayer.score}`
+            document.querySelector(`div[data-id="${id}"]`).setAttribute('data-score', backEndPlayer.score)
+            const parenetDiv = document.querySelector('#playerLabels')
+            const childDivs = Array.from(parenetDiv.querySelectorAll('div'))
+            childDivs.sort((a, b) => {
+                return b.getAttribute('data-score') - a.getAttribute('data-score')
+            })
 
-      childDivs.forEach((div) => {
-        parenetDiv.removeChild(div)
-      })
+            childDivs.forEach((div) => {
+                parenetDiv.removeChild(div)
+            })
 
-      childDivs.forEach((div) => {
-        parenetDiv.appendChild(div)
-      })
+            childDivs.forEach((div) => {
+                parenetDiv.appendChild(div)
+            })
 
-      if (id === socket.id) {
-        // if the player is the current player
+            if (id === socket.id) {
+                // if the player is the current player
 
-      } else {
-        // for all other players
-        frontEndPlayers[id].angle = backEndPlayer.angle
+            } else {
+                // for all other players
+                frontEndPlayers[id].angle = backEndPlayer.angle
 
-        gsap.to(frontEndPlayers[id], {
-          x: backEndPlayer.x,
-          y: backEndPlayer.y,
-          duration: 0.033,
-          ease: 'linear'
-        })
+                gsap.to(frontEndPlayers[id], {
+                    x: backEndPlayer.x,
+                    y: backEndPlayer.y,
+                    duration: 0.033,
+                    ease: 'linear'
+                })
 
-      }
+            }
+        }
     }
-  }
 
-  // delete frontend players
-  for (const id in frontEndPlayers) {
-    if (!backEndPlayers[id]) {
-      const divToDelete = document.querySelector(`div[data-id="${id}"]`)
-      divToDelete.parentNode.removeChild(divToDelete)
+    // delete frontend players
+    for (const id in frontEndPlayers) {
+        if (!backEndPlayers[id]) {
+            const divToDelete = document.querySelector(`div[data-id="${id}"]`)
+            divToDelete.parentNode.removeChild(divToDelete)
 
-      if (id === socket.id) {
-        // if the player is the current player
-        document.querySelector('#usernameForm').style.display = 'block'
-      }
-      delete frontEndPlayers[id]
+            if (id === socket.id) {
+                // if the player is the current player
+                document.querySelector('#usernameForm').style.display = 'block'
+            }
+            delete frontEndPlayers[id]
+        }
     }
-  }
-  //console.log(frontEndPlayers)
+    //console.log(frontEndPlayers)
 })
 
 socket.on('updateChatMessages', (backEndChatMessages) => {
-  frontEndChatMessages = backEndChatMessages;
+    frontEndChatMessages = backEndChatMessages;
 })
 
 let messagePoolTimer
 socket.on('updateChatMessagePool', ({ id, inputText }) => {
-  frontEndChatMessagePool.push({ id, inputText });
-  document.getElementById("messageList").innerHTML += `\n${frontEndPlayers[id].username}: ${inputText}`;// note: innerText will not parse html tags into text
-  document.getElementById("messageList").style.display = "block";
+    frontEndChatMessagePool.push({ id, inputText });
+    document.getElementById("messageList").innerHTML += `\n${frontEndPlayers[id].username}: ${inputText}`;// note: innerText will not parse html tags into text
+    document.getElementById("messageList").style.display = "block";
 
-  const messageList = document.getElementById("messageList");
-  messageList.scrollTop = messageList.scrollHeight;
-  clearTimeout(messagePoolTimer);
-  messagePoolTimer = setTimeout(() => {
-    document.getElementById("messageList").style.display = "none";
-  }, 5000);
+    const messageList = document.getElementById("messageList");
+    messageList.scrollTop = messageList.scrollHeight;
+    clearTimeout(messagePoolTimer);
+    messagePoolTimer = setTimeout(() => {
+        document.getElementById("messageList").style.display = "none";
+    }, 5000);
 })
 
 let animationId
 function animate() {
-  var playerPosition = {
-    x: 0,
-    y: 0
-  }
-  for (const id in frontEndPlayers) {
-    if (id === socket.id) {
-      playerPosition = {
-        x: frontEndPlayers[id].x,
-        y: frontEndPlayers[id].y,
-      }
+    var playerPosition = {
+        x: 0,
+        y: 0
     }
-  }
-  c.translate(-(playerPosition.x - canvas.width / 2), -(playerPosition.y - canvas.height / 2))
-  animationId = requestAnimationFrame(animate)
-  c.fillStyle = 'rgba(32, 32, 32, 1)'
-  c.fillRect(-2500, -2500, 5000, 5000)
+    for (const id in frontEndPlayers) {
+        if (id === socket.id) {
+            playerPosition = {
+                x: frontEndPlayers[id].x,
+                y: frontEndPlayers[id].y,
+            }
+        }
+    }
+    c.translate(-(playerPosition.x - canvas.width / 2), -(playerPosition.y - canvas.height / 2))
+    animationId = requestAnimationFrame(animate)
+    c.fillStyle = 'rgba(32, 32, 32, 1)'
+    c.fillRect(-2500, -2500, 5000, 5000)
 
-  for (const id in frontEndProjectiles) {
-    const projectile = frontEndProjectiles[id]
-    projectile.draw()
-  }
+    for (const id in frontEndProjectiles) {
+        const projectile = frontEndProjectiles[id]
+        projectile.draw()
+    }
 
-  for (const id in frontEndPlayers) {
-    const player = frontEndPlayers[id]
-    player.draw()
-  }
+    for (const id in frontEndPlayers) {
+        const player = frontEndPlayers[id]
+        player.draw()
+    }
 
-  colliders.forEach(element => {
-    element.draw()
-  });
+    colliders.forEach(element => {
+        element.draw()
+    });
 
-  for (const id in frontEndChatMessages) {
-    const frontEndChatMessage = frontEndChatMessages[id];
-    frontEndPlayers[id].drawBubbleMessage(frontEndChatMessage);
-  }
-  c.translate((playerPosition.x - canvas.width / 2), (playerPosition.y - canvas.height / 2))
+    for (const id in frontEndChatMessages) {
+        const frontEndChatMessage = frontEndChatMessages[id];
+        frontEndPlayers[id].drawBubbleMessage(frontEndChatMessage);
+    }
+    c.translate((playerPosition.x - canvas.width / 2), (playerPosition.y - canvas.height / 2))
 }
 animate()
 
 // keyfeedback
 const keys = {
-  w: {
-    pressd: false
-  },
-  a: {
-    pressd: false
-  },
-  s: {
-    pressd: false
-  },
-  d: {
-    pressd: false
-  }
+    w: {
+        pressd: false
+    },
+    a: {
+        pressd: false
+    },
+    s: {
+        pressd: false
+    },
+    d: {
+        pressd: false
+    }
 }
 
 const SPEED = 5
 const RADIUS = 40
 setInterval(() => {
-  // update projectiles
-  for (const id in frontEndProjectiles) {
-    const projectile = frontEndProjectiles[id]
-    const prePosition = {
-      x: projectile.x,
-      y: projectile.y
-    }
-    projectile.update()
-    // 边界判定（附带碰撞次数判定）
-    if (projectile.x < -map.width / 2 || projectile.x > map.width / 2 || projectile.y < -map.height / 2 || projectile.y > map.height / 2 || projectile.collisonCount > projectile.maxCollisonCount) {
-      frontEndPlayers[frontEndProjectiles[id].playerId].projectileCount--
-      delete frontEndProjectiles[id]
-    }
-    // 碰撞判定
-    for (var i = 0; i < colliders.length; i++) {
-      if (colliders[i].isColliding(projectile.x, projectile.y)) {// 子弹碰撞到障碍物
-        projectile.collisonCount++
-        const preVelocity = {
-          x: projectile.velocity.x,
-          y: projectile.velocity.y
+    // update projectiles
+    for (const id in frontEndProjectiles) {
+        const projectile = frontEndProjectiles[id]
+        const prePosition = {
+            x: projectile.x,
+            y: projectile.y
         }
-        const normal = colliders[i].getNormal(prePosition.x, prePosition.y)
-        projectile.x = prePosition.x
-        projectile.y = prePosition.y
-        projectile.velocity.x = preVelocity.x - 2 * normal.x * (preVelocity.x * normal.x + preVelocity.y * normal.y)
-        projectile.velocity.y = preVelocity.y - 2 * normal.y * (preVelocity.x * normal.x + preVelocity.y * normal.y)
-        break
-      }
-    }
-    // 命中判定
-    for (const frontEndPlayerId in frontEndPlayers) {
-      const frontEndPlayer = frontEndPlayers[frontEndPlayerId]
-      const distance = Math.hypot(projectile.x - frontEndPlayer.x, projectile.y - frontEndPlayer.y)
-      if (distance < RADIUS) {
-        frontEndPlayers[frontEndProjectiles[id].playerId].projectileCount--
-        delete frontEndProjectiles[id]
-        if (frontEndPlayerId === socket.id) {
-          socket.emit('hit', projectile.playerId)
-          break
+        projectile.update()
+        // 边界判定（附带碰撞次数判定）
+        if (projectile.x < -map.width / 2 || projectile.x > map.width / 2 || projectile.y < -map.height / 2 || projectile.y > map.height / 2 || projectile.collisonCount > projectile.maxCollisonCount) {
+            frontEndPlayers[frontEndProjectiles[id].playerId].projectileCount--
+            delete frontEndProjectiles[id]
         }
-      }
+        // 碰撞判定
+        for (var i = 0; i < colliders.length; i++) {
+            if (colliders[i].isColliding(projectile.x, projectile.y)) {// 子弹碰撞到障碍物
+                projectile.collisonCount++
+                const preVelocity = {
+                    x: projectile.velocity.x,
+                    y: projectile.velocity.y
+                }
+                const normal = colliders[i].getNormal(prePosition.x, prePosition.y)
+                projectile.x = prePosition.x
+                projectile.y = prePosition.y
+                projectile.velocity.x = preVelocity.x - 2 * normal.x * (preVelocity.x * normal.x + preVelocity.y * normal.y)
+                projectile.velocity.y = preVelocity.y - 2 * normal.y * (preVelocity.x * normal.x + preVelocity.y * normal.y)
+                break
+            }
+        }
+        // 命中判定
+        for (const frontEndPlayerId in frontEndPlayers) {
+            const frontEndPlayer = frontEndPlayers[frontEndPlayerId]
+            const distance = Math.hypot(projectile.x - frontEndPlayer.x, projectile.y - frontEndPlayer.y)
+            if (distance < RADIUS) {
+                frontEndPlayers[frontEndProjectiles[id].playerId].projectileCount--
+                delete frontEndProjectiles[id]
+                if (frontEndPlayerId === socket.id) {
+                    socket.emit('hit', projectile.playerId)
+                    break
+                }
+            }
+        }
     }
-  }
-  // update playerInputs
-  if (frontEndPlayers[socket.id]) {
-    const prePosition = {
-      x: frontEndPlayers[socket.id].x,
-      y: frontEndPlayers[socket.id].y,
-      angle: frontEndPlayers[socket.id].angle
+    // update playerInputs
+    if (frontEndPlayers[socket.id]) {
+        const prePosition = {
+            x: frontEndPlayers[socket.id].x,
+            y: frontEndPlayers[socket.id].y,
+            angle: frontEndPlayers[socket.id].angle
+        }
+        frontEndPlayers[socket.id].update(keys)
+        // 碰撞判定
+        colliders.forEach(element => {
+            if (element.isColliding(frontEndPlayers[socket.id].x + 25 * Math.cos(frontEndPlayers[socket.id].angle) - 35 * Math.sin(frontEndPlayers[socket.id].angle),
+                frontEndPlayers[socket.id].y + 25 * Math.sin(frontEndPlayers[socket.id].angle) + 35 * Math.cos(frontEndPlayers[socket.id].angle)) ||
+                element.isColliding(frontEndPlayers[socket.id].x - 25 * Math.cos(frontEndPlayers[socket.id].angle) - 35 * Math.sin(frontEndPlayers[socket.id].angle),
+                    frontEndPlayers[socket.id].y - 25 * Math.sin(frontEndPlayers[socket.id].angle) + 35 * Math.cos(frontEndPlayers[socket.id].angle)) ||
+                element.isColliding(frontEndPlayers[socket.id].x + 25 * Math.cos(frontEndPlayers[socket.id].angle) + 35 * Math.sin(frontEndPlayers[socket.id].angle),
+                    frontEndPlayers[socket.id].y + 25 * Math.sin(frontEndPlayers[socket.id].angle) - 35 * Math.cos(frontEndPlayers[socket.id].angle)) ||
+                element.isColliding(frontEndPlayers[socket.id].x - 25 * Math.cos(frontEndPlayers[socket.id].angle) + 35 * Math.sin(frontEndPlayers[socket.id].angle),
+                    frontEndPlayers[socket.id].y - 25 * Math.sin(frontEndPlayers[socket.id].angle) - 35 * Math.cos(frontEndPlayers[socket.id].angle))) {
+                frontEndPlayers[socket.id].x = prePosition.x
+                frontEndPlayers[socket.id].y = prePosition.y
+                frontEndPlayers[socket.id].angle = prePosition.angle
+            }
+        })
+        socket.emit('updatePlayers', {
+            x: frontEndPlayers[socket.id].x,
+            y: frontEndPlayers[socket.id].y,
+            angle: frontEndPlayers[socket.id].angle
+        })
     }
-    frontEndPlayers[socket.id].update(keys)
-    // 碰撞判定
-    colliders.forEach(element => {
-      if (element.isColliding(frontEndPlayers[socket.id].x + 25 * Math.cos(frontEndPlayers[socket.id].angle) - 35 * Math.sin(frontEndPlayers[socket.id].angle),
-        frontEndPlayers[socket.id].y + 25 * Math.sin(frontEndPlayers[socket.id].angle) + 35 * Math.cos(frontEndPlayers[socket.id].angle)) ||
-        element.isColliding(frontEndPlayers[socket.id].x - 25 * Math.cos(frontEndPlayers[socket.id].angle) - 35 * Math.sin(frontEndPlayers[socket.id].angle),
-          frontEndPlayers[socket.id].y - 25 * Math.sin(frontEndPlayers[socket.id].angle) + 35 * Math.cos(frontEndPlayers[socket.id].angle)) ||
-        element.isColliding(frontEndPlayers[socket.id].x + 25 * Math.cos(frontEndPlayers[socket.id].angle) + 35 * Math.sin(frontEndPlayers[socket.id].angle),
-          frontEndPlayers[socket.id].y + 25 * Math.sin(frontEndPlayers[socket.id].angle) - 35 * Math.cos(frontEndPlayers[socket.id].angle)) ||
-        element.isColliding(frontEndPlayers[socket.id].x - 25 * Math.cos(frontEndPlayers[socket.id].angle) + 35 * Math.sin(frontEndPlayers[socket.id].angle),
-          frontEndPlayers[socket.id].y - 25 * Math.sin(frontEndPlayers[socket.id].angle) - 35 * Math.cos(frontEndPlayers[socket.id].angle))) {
-        frontEndPlayers[socket.id].x = prePosition.x
-        frontEndPlayers[socket.id].y = prePosition.y
-        frontEndPlayers[socket.id].angle = prePosition.angle
-      }
-    })
-    socket.emit('updatePlayers', {
-      x: frontEndPlayers[socket.id].x,
-      y: frontEndPlayers[socket.id].y,
-      angle: frontEndPlayers[socket.id].angle
-    })
-  }
 }, 33)
 
